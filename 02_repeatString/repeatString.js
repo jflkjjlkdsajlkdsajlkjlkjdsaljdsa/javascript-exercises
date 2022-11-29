e3bb8f6d928409430545adae336c525731b5675c
Admin <47427831+jflkjjlkdsajlkdsajlkjlkjdsaljdsa@users.noreply.github.com>
2022-11-29 15:47:20 +0100

    Lösung V1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let str = "";
+    for (let i = 0; i < num; i++ ){
+    str = str + string
+    }
+    return str;
 };
 
 // Do not edit below this line
