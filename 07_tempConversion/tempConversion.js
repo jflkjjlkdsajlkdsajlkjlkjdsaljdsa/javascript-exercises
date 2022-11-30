18e2b6df8870bbf0cb9163d57507433d04b2bb6f
Admin <47427831+jflkjjlkdsajlkdsajlkjlkjdsaljdsa@users.noreply.github.com>
2022-11-30 14:00:00 +0100

    V1 Lösung Fahrenheit/Celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let tempC =  (temperature -32)* 5 /9; 
+
+  return Math.round(tempC*10)/10
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temperature) {
+  let tempF = (temperature *1.8) + 32;
+
+  return Math.round(tempF*10)/10;
+}
 
 // Do not edit below this line
 module.exports = {
