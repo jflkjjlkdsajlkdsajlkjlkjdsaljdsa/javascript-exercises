364cae8697f6c4531a7d4e9756945e3787e1df0d
Admin <47427831+jflkjjlkdsajlkdsajlkjlkjdsaljdsa@users.noreply.github.com>
2022-12-12 15:14:50 +0100

    V1 Lösung

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(myArray) {
+     x = [];
+     let c =[];
+     let a;
+     let d = myArray;
+     console.log(myArray.length)
+     console.log(typeof myArray[2].yearOfDeath)
+     for (i = 0; i< myArray.length ; i++){
+        if (typeof myArray[i].yearOfDeath === 'undefined'){
+            a = new Date().getFullYear() 
+        }
+        else {
+            a = myArray[i].yearOfDeath;
+        }
+
+        let b = myArray[i]["yearOfBirth"];
+        let c = a-b; 
+        x.push(c); 
+     }
+     let max = Math.max(...x);
+     let index = x.indexOf(max);
+     let result =  d[index];
+return result;    
 
 };
 
