3abf3e852169edc371b818f9d4d11ad8c016b5e1
Admin <47427831+jflkjjlkdsajlkdsajlkjlkjdsaljdsa@users.noreply.github.com>
2022-12-12 11:50:17 +0100

    Lösung

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = parseInt(x);
+    if (x > 0) {
+        if (typeof x === "number") {
+            let fibonacciArr = [1, 1];
+            y = x - 2;
+            let nextValue = 0;
+            for (i = 0; i < y; i++) {
+                z = i + 1;
+                nextValue = fibonacciArr[i] + fibonacciArr[z];
+                fibonacciArr.push(nextValue);
+            }
+            return fibonacciArr[x - 1];
+        }
+    } else if (x < 0) {
+        return "OOPS"
+    } else {
+        return false;
+    }
 
 };
 
 // Do not edit below this line
-module.exports = fibonacci;
+module.exports = fibonacci
